7449c9c6d6b6d35656e9c50352617c1d1061462d
deeps121890 <deepikadeepu8818@gmail.com>
2024-12-18 14:38:10 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,15 @@
+'''
+def detail(x,y):
+    return f'{x},{y}'
+res=details(10,20)
+print(res)
+'''
+o/p:10,20
+'''
+def detail(x,y):
+    return f'sum of{x},and{y} is {x+y}'
+res=details(10,20)
+print(res)
+'''
+o/p:30
+
