65d02cd1ec9131a2c4ca8877bb75d6ea7c64b93a
deeps121890 <deepikadeepu8818@gmail.com>
2024-12-18 14:45:18 +0530

    Create conditional statements.py

---
+++ conditional statements.py
@@ -0,0 +1,24 @@
+'''
+x=10
+if x>5:
+    print('hello')
+'''
+o/p: 
+'''
+x=10
+if x!=10:
+    print('hello')
+print('bye') 
+'''
+o/p: bye
+'''
+x=int(input())
+if x%2==0:
+    print('even number')
+else:
+    print('odd number')
+'''
+o/p:40 
+    even number
+
+
